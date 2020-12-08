80a0304ffc673e37ebb671fec83d79023f3d782e
doujiaming <690772293@qq.com>
2020-12-08 15:40:01 +0800

    diwugebanben

--- miao/lodash/doujm6221728-lodash.js
+++ miao/lodash/doujm6221728-lodash.js
@@ -1,8 +1,10 @@
-var doujm = {
+var doujm6221728 = {
   compact: function (array) {
     var result = []
     for (var i = 0; i < array.length; i++) {
-      result.push(array[i])
+      if (array[i]) {
+        result.push(array[i])
+      }
     }
     return result
   } 
